4a34831e8c266c3ad99c911c14e4303185f7efef
fatima.zafar <fatima.zafar@TS-WKS-44>
2024-07-09 15:59:57 +0500

    Add project files.

---
+++ W2-D2-01/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using W2_D2_01.Models;
+using static W2_D2_01.Models.Pizza;
+
+namespace W2_D2_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var _pizza = new Pizza();
+            return View(_pizza);
+        }
+        [HttpPost]
+        public IActionResult Index(Pizza model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Process the selected pizza options
+                return RedirectToAction("Index", model);
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ W2-D2-01/Models/Pizza.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace W2_D2_01.Models
+{
+    public enum SizeEnum
+    {
+        Small,
+        Medium,
+        Large
+    }
+    public enum BaseEnum
+    {
+        Thick,
+        Thin
+    }
+    public enum ToppingEnum
+    {
+        Pepperoni=1,
+        Chicken=2,
+        Extra_Cheese=3,
+        Mushrooms=4,
+        Spinach=5,
+        Olives=6
+
+    }
+
+    public class Pizza
+    {
+        
+        [Required]
+        public SizeEnum Size { get; set; }
+        [Required]
+        public BaseEnum Base { get; set; }
+        [Required]
+        public List<ToppingEnum> Toppings { get; set; }
+    } 
+}
